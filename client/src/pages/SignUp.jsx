7902731337c97350641238941c2db4ae697f7707
Dispenser254 <stevensonkibs55@gmail.com>
2024-05-09 23:30:44 +0300

    Complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export function SignUp() {
   const [formData, setFormData] = useState();
@@ -10,7 +11,7 @@ export function SignUp() {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: [e.target.value.trim()] });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
   const handleSubmit = async (e) => {
@@ -107,6 +108,7 @@ export function SignUp() {
                 "Sign Up"
               )}
             </Button>
+            <OAuth />
           </form>
 
           <div className="flex gap-2 text-sm mt-5">
