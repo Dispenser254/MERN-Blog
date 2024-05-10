eaf7a9980299cb3f9f58215ae80ec4a8675ad758
Dispenser254 <stevensonkibs55@gmail.com>
2024-05-10 13:23:49 +0300

    Complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,20 +1,20 @@
 // import React from 'react'
 
-import { Sidebar } from "flowbite-react"
+import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 
 function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState("");
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get("tab");
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-    }, [location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
@@ -25,6 +25,7 @@ function DashSidebar() {
               icon={HiUser}
               label="User"
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
@@ -39,4 +40,4 @@ function DashSidebar() {
   );
 }
 
-export default DashSidebar
+export default DashSidebar;
