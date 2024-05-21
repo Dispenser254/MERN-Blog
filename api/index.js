ba33586c202a04114e7c7e58a3597134168e8b27
Dispenser254 <stevensonkibs55@gmail.com>
2024-05-21 23:43:28 +0300

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "../api/routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import postRoutes from "../api/routes/post.route.js";
 import commentRoutes from "../api/routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -32,6 +35,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((error, request, response, next) => {
   const statusCode = error.statusCode || 500;
